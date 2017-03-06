a4854f393eebdcfbb9e13143bf22d367730506bc
world-tkd <1253171450@qq.com>
2017-03-06 23:24:13 +0800

    123456

---
+++ 161115_ReactDemo-master/work/hello-comment/src/components/list/list.js
@@ -0,0 +1,43 @@
+import React, {Component, PropTypes} from 'react'
+import Item from '../item/item'
+import './list.css'
+
+/**
+ * Created by xfzhang on 2017/3/6.
+ */
+export default class List extends Component {
+
+
+  render () {
+    const {comments, remove} = this.props
+    //计算提示文本的显示性
+    let display = comments.length===0 ? 'block' : 'none'
+    const items = comments.map((comment, index) => {
+      const itemProps = {
+        key: index,
+        comment,
+        remove,
+        index
+      }
+      return <Item {...itemProps}/>
+    })
+    return (
+      <div className="col-md-8">
+        <h3 className="reply">评论回复：</h3>
+        <h2 style={{display}}>暂无评论，点击左侧添加评论！！！</h2>
+        <ul className="list-group">
+          {items}
+        </ul>
+      </div>
+    )
+  }
+}
+List.propTypes = {
+  comments: PropTypes.array.isRequired,
+  remove: PropTypes.func.isRequired
+}
+
+comments.map(() => {
+
+  return <Item key ={index} index ={index} remove = {remove} comment = {comment}/>
+})
